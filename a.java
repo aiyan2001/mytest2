a430f3882ce88b249c510034e7a2bd55280b43a6
aiyan2001 <aiyan2001@sohu.com>
2015-04-11 09:47:50 -0700

    add a.java in master

--- a.java
+++ a.java
@@ -1,5 +1,6 @@
 public static void main(String[] argv){
+
 	System.out.println("hello world");
         System.out.println("add line...in dev branch!")
-;
+        System.out.println("add in master");
 }
