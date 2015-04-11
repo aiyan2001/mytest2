3459fa24279cca6e483eddb5964c214d3c9a2255
aiyan2001 <aiyan2001@sohu.com>
2015-04-11 09:34:44 -0700

    add feature1

--- a.java
+++ a.java
@@ -1,4 +1,5 @@
 public static void main(String[] argv){
+        System.out.println("add line ...in feature1");
 	System.out.println("hello world");
         System.out.println("add line...in dev branch!")
 ;
