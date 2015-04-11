3a462a6ff9b8028267c6766c06f341d3e2aa11fb
aiyan2001 <aiyan2001@sohu.com>
2015-04-11 10:23:02 -0700

    add a.java 2

--- a.java
+++ a.java
@@ -3,4 +3,5 @@ public static void main(String[] argv){
 	System.out.println("hello world");
         System.out.println("add line...in dev branch!")
         System.out.println("add in master");
+        System.out.println("add in master 2");
 }
